f78c38f8bd306bce1affda6eb7684729cfed7c6f
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2019-06-12 18:19:28 +0300

    removed stuff

--- scripts/copy-nmap-wrapper-files.sh
+++
@@ -1,7 +0,0 @@
-cp ../local-data-collector/setup-nmap-wrapper.py ./setup.py
-
-mkdir -p nmapwrapper/network nmapwrapper/clientserver
-
-cp -avr ../local-data-collector/datacollector/nmapnetwork/* nmapwrapper/network/
-
-cp -avr ../local-data-collector/ext/tcp-client-server/clientserver/* nmapwrapper/clientserver/
